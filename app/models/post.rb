df01b1a3ace06c35a707dc11408e002b6b6837eb
BoTung <bver49@gmail.com>
2016-06-12 15:49:40 +0800

    fix tag function

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,10 +2,10 @@ class Post < ActiveRecord::Base
 	belongs_to :user
 	has_many :comments, dependent: :destroy
 	has_many :likes, dependent: :destroy
+	has_many :taggings
+	has_many :tags,through: :taggings
 
 	mount_uploader :image, ImageUploader
-
-	acts_as_taggable
 
 	validates_presence_of :title , :message => "請填寫標題"
 	validates_presence_of :image , :message => "請上傳圖片"
@@ -13,5 +13,23 @@ class Post < ActiveRecord::Base
 
 	def likeamt
 		Like.where(post_id: id).count
+	end
+
+	def self.tagged_with(name)
+		Tag.find_by_name!(name).posts
+	end
+
+	def self.tag_counts
+		Tag.select("tags.*, count(taggings.tag_id) as count").joins(:taggings).group("taggings.tag_id")
+	end
+
+	def tag_list
+		tags.map(&:name).join(", ")
+	end
+
+	def tag_list=(names)
+		self.tags = names.split(",").map do |n|
+			Tag.where(name: n.strip).first_or_create!
+		end
 	end
 end
