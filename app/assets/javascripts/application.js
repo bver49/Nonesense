dec431d4d232fb00554610fb14d0163810ee15ff
BoTung <bver49@gmail.com>
2016-07-25 17:46:28 +0800

    fix scroll top

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,23 +19,3 @@
 //= require bootstrap-sprockets
 //= require turbolinks
 //= require_tree .
-
-$(document).ready(function(){
-
-	$('.scrollToTop').hide();
-	//Check to see if the window is top if not then display button
-	$(window).scroll(function(){
-		if ($(this).scrollTop() > 100) {
-			$('.scrollToTop').fadeIn();
-		} else {
-			$('.scrollToTop').fadeOut();
-		}
-	});
-
-	//Click event to scroll to top
-	$('.scrollToTop').click(function(){
-		$('html, body').animate({scrollTop : 0},800);
-		return false;
-	});
-
-});
