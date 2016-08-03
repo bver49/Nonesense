757ec6a91ac9a9087ecd8623be8f2d354b8d6853
BoTung <bver49@gmail.com>
2016-08-03 19:28:15 +0800

    fix bug

--- app/helpers/draws_helper.rb
+++ app/helpers/draws_helper.rb
@@ -1,2 +1,5 @@
 module DrawsHelper
+  def check_today_draw
+    @draw = Draw.where('user_id = ? AND created_at BETWEEN ? AND ?',current_user.id, DateTime.now.beginning_of_day, DateTime.now.end_of_day).length
+  end
 end
