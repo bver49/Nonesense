987389409a6d0244ba60755a1e9e0b3ad9f34fbc
BoTung <bver49@gmail.com>
2016-08-24 23:32:09 +0800

    change logo

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160813071753) do
+ActiveRecord::Schema.define(version: 20160813074333) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "body"
